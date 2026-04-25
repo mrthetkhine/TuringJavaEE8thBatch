6525a98062733000c7339d1888dc627d95ff8728
thetkhine <macbook@MacBooks-MacBook-Pro.local>
2026-04-25 20:21:46 +0630

    add JPQL

--- javaee8-jpa-mvc/src/main/java/com/turing/javaee8/jpamvc/repository/MovieDao.java
+++ javaee8-jpa-mvc/src/main/java/com/turing/javaee8/jpamvc/repository/MovieDao.java
@@ -1,8 +1,11 @@
 package com.turing.javaee8.jpamvc.repository;
 
+import java.util.Collection;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.turing.javaee8.jpamvc.model.Movie;
@@ -10,5 +13,34 @@ import com.turing.javaee8.jpamvc.model.Movie;
 @Repository
 public interface MovieDao extends JpaRepository<Movie, Long>{
 	List<Movie> findByTitle(String title);
+	List<Movie> findByTitleLike(String title);
+	List<Movie> findByTitleIgnoreCase(String title);
 	List<Movie> findByGenreAndYear(String genre,Integer year);
+	List<Movie> findByGenreOrderByYearDesc(String genre);
+	List<Movie> findByYearLessThan(Integer year);
+	List<Movie> findByYearAfter(Integer year);
+	List<Movie> findByGenreNot(String genre);
+	List<Movie> findByGenreIn(Collection<String> genres);
+	
+	@Query("select m from Movie m where m.id = ?1/*here is comment*/")
+	Movie getMovieById(Long id);
+	
+	@Query("select m from Movie m where m.year=:year")
+	List<Movie> getMovieByYear(Integer year);
+	
+	@Modifying
+	@Query("update Movie m set m.genre = :genre where m.id = :id")
+	int updateMovieGenreById(Long id, String genre);
+	
+	@Modifying
+	@Query("delete Movie m where m.id = :id")
+	int deleteMovieByid(Long id);
+	
+	@Modifying
+	@Query("insert into Movie(title,year,genre) values(:title,:year,:genre)")
+	int insertMovie(String title, int year,String genre);
+	
+	//@Query("from Movie m")
+	@Query("select m from Movie m")
+	List<Movie> getAllMovies();
 }
--- javaee8-jpa-mvc/src/test/java/com/turing/javaee8/jpamvc/repository/querymethod/MovieQueryTest.java
+++ javaee8-jpa-mvc/src/test/java/com/turing/javaee8/jpamvc/repository/querymethod/MovieQueryTest.java
@@ -1,10 +1,14 @@
 package com.turing.javaee8.jpamvc.repository.querymethod;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 
 import com.turing.javaee8.jpamvc.model.Movie;
@@ -22,12 +26,51 @@ public class MovieQueryTest {
 	@Autowired
 	MovieDao movieDao;
 
-	@Test
+	//@Test
 	@Transactional
 	public void testQueryByTitle()
 	{
-		//List<Movie> movies = this.movieDao.findByTitle("Titanic");
-		List<Movie> movies = this.movieDao.findByGenreAndYear("Drama",2005);
-		movies.forEach(System.out::println);
+		//List<Movie> movies = this.movieDao.findByTitleIgnoreCase("Tit");
+		//List<Movie> movies = this.movieDao.findByGenreAndYear("Drama",2005);
+		//List<Movie> movies = this.movieDao.findByGenreOrderByYearDesc("Drama");
+		//List<Movie> movies = this.movieDao.findByYearAfter(2000);
+		//List<Movie> movies = this.movieDao.findByTitleLike("%men");
+		//List<Movie> movies = this.movieDao.findByGenreNot("Drama");
+		List<String> genres =new ArrayList<>();
+		genres.add("Crime");
+		genres.add("Action");
+		List<Movie> movies = this.movieDao.findByGenreIn(genres);
+		movies.forEach(System.err::println);
+	}
+	
+	//@Test
+	@Transactional
+	public void testPagination()
+	{
+		PageRequest page = PageRequest.of(1, 5);
+		Page<Movie> movies  = this.movieDao.findAll(page);
+		movies.forEach(System.err::println);
+	}
+	//@Test
+	@Transactional
+	public void testSort()
+	{
+		
+		List<Movie> movies  = this.movieDao.findAll(Sort.by("year").descending());
+		movies.forEach(System.err::println);
+	}
+	
+	@Test
+	@Transactional
+	public void testJPQL()
+	{
+		//int effectedRow = this.movieDao.updateMovieGenreById(17L, "Crime");
+		//int effectedRow = this.movieDao.deleteMovieByid(17L);
+		//int effectedRow = this.movieDao.insertMovie("Test",200,"Drama");
+		//System.out.println("Row effected "+effectedRow);
+		
+		//Movie movie =this.movieDao.getMovieById(2L);
+		List<Movie> movies  = this.movieDao.getAllMovies();
+		movies.forEach(System.err::println);
 	}
 }
